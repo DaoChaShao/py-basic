ed05a5ed1477dbbb2c7ae23b644f948323b180b1
DaoChaShao <27882272@qq.com>
2025-10-24 23:01:47 +0800

    chore: add Python script demonstrating anonymous functions without parameters

---
+++ 20200219 01 匿名函数 01 无参数.py
@@ -0,0 +1,18 @@
+
+# 不接受参数
+
+
+# 传统函数
+def func_01():
+    print('Hello World')
+
+
+func_01()
+
+
+# 匿名函数
+func_02 = lambda: print('Hello World')
+
+func_02()
+
+print(lambda: print('Hello World'))
