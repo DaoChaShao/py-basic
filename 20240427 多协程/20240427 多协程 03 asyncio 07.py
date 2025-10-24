efbb2f070578e1edc7a6e72f5c87746aa6c2a63a
DaoChaShao <27882272@qq.com>
2025-10-24 23:18:54 +0800

    chore: add asyncio example with names and numbers functions

---
+++ 20240427 多协程/20240427 多协程 03 asyncio 07.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     : 2024/12/27 18:51
+# @Author   : Shawn
+# @Version  : V 1.0
+# @Fime     : 20240427 多协程 03 asyncio 07.py
+# @Desc     :
+
+from asyncio import create_task, sleep, wait, run
+from faker import Faker
+from random import uniform
+
+
+async def names(faker: Faker, amount: int, wait_min: float, wait_max: float, delay_default: int) -> None:
+    """ Seeker Function """
+    _names = [faker.name() for _ in range(amount)]
+
+    for i, _name in enumerate(_names):
+        # Simulate the time it takes to get the data via the network
+        await sleep(uniform(wait_min, wait_max))
+        # Simulate the time it takes to process the data
+        await sleep(delay_default)
+        print(f"Names {i + 1:>02d}: {_name}")
+
+
+async def numbers(faker: Faker, amount: int, wait_min: float, wait_max: float, delay_default: int) -> None:
+    """ Demonstrator Function """
+    _numbers = [faker.phone_number() for _ in range(amount)]
+
+    for j, _number in enumerate(_numbers):
+        # Simulate the time it takes to get the data via the network
+        await sleep(uniform(wait_min, wait_max))
+        # Simulate the time it takes to process the data
+        await sleep(delay_default)
+        print(f"Telephones {j + 1:>02d}: {_number}")
+
+
+def main() -> None:
+    """ Main Function """
+    data_faker = Faker()
+
+    WAIT_MIN: float = 0.5
+    WAIT_MAX: float = 1.5
+    DELAY_DEFAULT: int = 1
+    AMOUNT_DATA: int = 3
+
+    tasks = [
+        names(data_faker, AMOUNT_DATA, WAIT_MIN, WAIT_MAX, DELAY_DEFAULT),
+        numbers(data_faker, AMOUNT_DATA, WAIT_MIN, WAIT_MAX, DELAY_DEFAULT),
+    ]
+
+    # python 3.8+ has been deprecated this method
+    done, pending = run(wait(tasks))
+    for task in done:
+        print(f"Task: {task.get_name()} is done.")
+
+
+if __name__ == "__main__":
+    main()
