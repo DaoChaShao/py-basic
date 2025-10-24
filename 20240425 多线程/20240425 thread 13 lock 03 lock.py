504bc11d1f517de8cc30984026737ff045c4aac3
DaoChaShao <27882272@qq.com>
2025-10-24 23:16:13 +0800

    chore: add example of thread synchronization using Lock in addition functions

---
+++ 20240425 多线程/20240425 thread 13 lock 03 lock.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     : 2024/12/27 15:57
+# @Author   : Shawn
+# @Version  : V 1.0
+# @Fime     : 20240425 thread 13 lock 03 lock.py
+# @Desc     :
+
+from threading import Thread, Lock
+
+NUMBER: int = 0
+
+
+def addition(amount: int, lock: Lock, thread_name: str) -> None:
+    """ Addition Function """
+    for _ in range(amount):
+        lock.acquire()
+        global NUMBER
+        NUMBER += 1
+        lock.release()
+    print(f"{thread_name} : {NUMBER}")
+
+
+def adder(amount: int, lock: Lock, thread_name: str) -> None:
+    """ Adder Function """
+    for _ in range(amount):
+        lock.acquire()
+        global NUMBER
+        NUMBER += 1
+        lock.release()
+    print(f"{thread_name} : {NUMBER}")
+
+
+def main() -> None:
+    """ Main Function """
+
+    AMOUNT_DATA: int = 1_000
+
+    lock: Lock = Lock()
+
+    tom = Thread(target=addition, args=(AMOUNT_DATA, lock, "Tom"))
+    jerry = Thread(target=adder, args=(AMOUNT_DATA, lock, "Jerry"))
+
+    tom.start()
+    jerry.start()
+
+
+if __name__ == "__main__":
+    main()
