1b494fa38abae3ce09a52d92489754861c901885
DaoChaShao <27882272@qq.com>
2025-10-24 23:16:42 +0800

    chore: add multiprocessing example with calculation and random delays

---
+++ 20240426 多进程/20240425 多线程 01.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     : 2024/12/26 19:44
+# @Author   : Shawn
+# @Version  : V 1.0
+# @Fime     : 20240425 多线程 01.py
+# @Desc     :
+
+from dataclasses import dataclass
+from multiprocessing import Process, current_process
+from random import randint, uniform
+from time import sleep
+
+
+class Calculation(object):
+    def __init__(self, x: list[int], y: list[int], s: list[float]) -> None:
+        self._x: list[int] = x
+        self._y: list[int] = y
+        self._s: list[float] = s
+
+    def __call__(self, *args, **kwargs):
+        current = current_process()
+
+        for i, (m, n, delay) in enumerate(zip(self._x, self._y, self._s)):
+            result: int = m + n
+            print(
+                f"{current.pid} {current.name:^12} is running: "
+                f"{i + 1:>02d} cal result: {result:>03d}."
+            )
+            sleep(delay)
+
+        print("The calculation is done.")
+
+
+@dataclass
+class Person(object):
+    amount: int
+    name: str
+
+    NUM_MIN: int = 1
+    NUM_MAX: int = 100
+    SECOND_MIN: float = 0
+    SECOND_MAX: float = 1
+
+    def __init__(self, amount: int, name: str) -> None:
+        self._amount: int = amount
+        self._name: str = name
+        self._x: list[int] = []
+        self._y: list[int] = []
+        self._t: list[float] = []
+
+    def __call__(self, *args, **kwargs):
+        self._x = [randint(self.NUM_MIN, self.NUM_MAX) for _ in range(self._amount)]
+        self._y = [randint(self.NUM_MIN, self.NUM_MAX) for _ in range(self._amount)]
+        self._t = [uniform(self.SECOND_MIN, self.SECOND_MAX) for _ in range(self._amount)]
+        self._process_starter()
+
+    def _process_starter(self):
+        calc = Calculation(self._x, self._y, self._t)
+        process = Process(target=calc, name=self._name)
+        process.start()
+
+        print(f"{self._name} start to calculate:")
+
+
+def main() -> None:
+    """ Main Function """
+    AMOUNT = 10
+
+    TOM = "Tom"
+    JERRY = "Jerry"
+
+    tom = Person(AMOUNT, TOM)
+    jerry = Person(AMOUNT, JERRY)
+
+    tom()
+    jerry()
+
+
+if __name__ == "__main__":
+    main()
