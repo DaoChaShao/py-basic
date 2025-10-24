da37f865a6e0f25f84e3245df322a659741558fe
DaoChaShao <27882272@qq.com>
2025-10-24 22:58:55 +0800

    chore: add software installation script using apt in Python

---
+++ background/06 软件安装.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2020/02/09 21:35
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   06 软件安装.py
+# @Desc     :   
+
+
+# 软件安装
+
+# 1.通过 apt 安装/卸载软件
+# - apt 是 Advanced Packaging Tool
+# - apt 是 Linux 下的一款安装包管理工具
+# - apt 可以在终端中方便 安装/卸载/更新软件包
+
+# 1.1 安装软件
+# sudo apt install 软件包
+
+# 1.2 卸载软件
+# sudo apt remove 软件名
+
+# 1.3 更新已安装的包
+# sudo apt upgrade
+
+# 2.软件安装
+
+# 2.1 一个小火车提示
+# sudo apt install sl
+
+# 2.2 一个比较漂亮的查看当前进程排名的软件
+# sudo apt install htop
