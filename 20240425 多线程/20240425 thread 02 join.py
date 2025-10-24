5e7e4fe101d1bfd7da14f1d4463251d20abb681f
DaoChaShao <27882272@qq.com>
2025-10-24 23:15:30 +0800

    chore: add multithreading example with thread monitoring and join functionality

---
+++ 20240425 多线程/20240425 thread 02 join.py
@@ -0,0 +1,48 @@
+import threading
+import time
+
+from utils.decorator import beautifier
+from utils.highlighter import lines
+
+
+@beautifier
+def current_thread_check():
+    '''
+        产看当前正在运行的线程数量
+        :return:
+        '''
+    print(f'现在正在运行的线程数量： {threading.active_count()}')  # 输出当前正在运行的线程数量
+    lines()
+    print(f'当前线程运行的所有线程： {threading.enumerate()}')  # 输出当前运行的所有线程
+    lines()
+    print(f'当前线程的名称：{threading.current_thread().name}')  # 输出当前线程的名称
+
+
+@beautifier
+def branch_thread_func_01():
+    print(f'延时线程：{threading.current_thread().name} 已经开始')
+    print(f'当前线程运行的所有线程： {threading.enumerate()}')  # 输出当前运行的所有线程
+    lines()
+    for i in range(10):
+        time.sleep(0.1)
+    lines()
+    print(f'延时线程：{threading.current_thread().name} 已经结束')
+    print(f'当前线程运行的所有线程： {threading.enumerate()}')  # 输出当前运行的所有线程
+
+
+def main():
+    current_thread_check()  # 调用函数查看当前线程数量
+
+    # 创建新线程，并将新线程功能与线程名称绑定（有点儿像pyside6的信号槽）
+    new_thread = threading.Thread(target=branch_thread_func_01, name='延时线程')
+    new_thread.start()  # 启动新线程
+
+    # 添加 join 函数
+    new_thread.join()  # 等待新线程结束，才执行主线程后续的任务
+
+    print(f'主线程结束')  # 要想让主线程后续的任务在新线程结束后执行，就需要添加 join() 函数 ↑
+    print(f'当前线程运行的所有线程： {threading.enumerate()}')  # 输出当前运行的所有线程
+
+
+if __name__ == '__main__':
+    main()
