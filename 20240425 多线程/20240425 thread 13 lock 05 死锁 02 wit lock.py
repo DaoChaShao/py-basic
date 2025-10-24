42a921e8c59ac5c32e3e605a42ed0f478dd2e6e7
DaoChaShao <27882272@qq.com>
2025-10-24 23:16:34 +0800

    chore: add example of thread synchronization with Lock in seeker function

---
+++ 20240425 多线程/20240425 thread 13 lock 05 死锁 02 wit lock.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     : 2024/12/27 16:43
+# @Author   : Shawn
+# @Version  : V 1.0
+# @Fime     : 20240425 thread 13 lock 05 死锁 02 wit lock.py
+# @Desc     :
+
+from faker import Faker
+from random import uniform
+from threading import Thread, Lock, current_thread
+from time import sleep
+
+
+def data_set(faker: Faker, amount: int) -> list[str]:
+    """ Data Set Function """
+    _names = [faker.first_name() for _ in range(amount)]
+    return _names
+
+
+def seeker(lock: Lock, index: int, names: list[str]) -> None:
+    """ Seeker Function """
+    with lock:
+        print(f"Current thread: {current_thread().ident} {current_thread().name}")
+
+        if index >= len(names):
+            print(f"End, the {index} does not exist.")
+            return
+        print(f"{names[index]:^12} is seeked.")
+
+        SLEEP_MIN: int = 0
+        SLEEP_MAX: int = 1
+        sleep(uniform(SLEEP_MIN, SLEEP_MAX))
+
+
+def main() -> None:
+    """ Main Function """
+    AMOUNT_DATA: int = 10
+    data_faker: Faker = Faker()
+
+    names: list[str] = data_set(data_faker, AMOUNT_DATA)
+
+    lock: Lock = Lock()
+
+    AMOUNT_THREAD: int = 20
+    for j in range(AMOUNT_THREAD):
+        thread = Thread(target=seeker, args=(lock, j, names), name=f"Thread-{j}")
+        thread.start()
+
+
+if __name__ == "__main__":
+    main()
