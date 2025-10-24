77faf7b6ea1a62831cd44cf118d0c167960f3dd5
DaoChaShao <27882272@qq.com>
2025-10-24 23:13:43 +0800

    chore: add Loop class for incrementing coordinates based on a specified amount

---
+++ 20240419 三器一式/20240419 21 loop 01.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     : 2025/1/2 01:40
+# @Author   : Shawn
+# @Version  : Version 1.0
+# @Fime     : 20240419 21 loop 01.py
+# @Desc     : 
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Loop():
+    _x: int
+    _y: int
+    _amount: int
+
+    def __post_init__(self):
+        for _ in range(self._amount):
+            self._x += 1
+        else:
+            self._y += 1
+
+    def __str__(self):
+        return f"{self._x}, {self._y}"
+
+
+def main() -> None:
+    """ Main Function """
+    X: int = 1
+    Y: int = 1
+    AMOUNT: int = 10
+    loop = Loop(X, Y, AMOUNT)
+    print(loop)
+
+
+if __name__ == "__main__":
+    main()
