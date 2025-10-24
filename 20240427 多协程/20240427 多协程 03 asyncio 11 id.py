482530c52b7305b6c5d3a8208d6c711da3dc1f39
DaoChaShao <27882272@qq.com>
2025-10-24 23:19:15 +0800

    chore: add asyncio-based calculation game with event handling

---
+++ 20240427 多协程/20240427 多协程 03 asyncio 11 id.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     : 2025/1/2 16:18
+# @Author   : Shawn
+# @Version  : Version 1.0
+# @Fime     : 20240427 多协程 03 asyncio 11 id.py
+# @Desc     : 
+
+from asyncio import run, sleep, gather, create_task, Event, current_task
+from dataclasses import dataclass
+from faker import Faker
+from time import perf_counter
+
+
+@dataclass
+class CalculationGame(object):
+    """ Calculation Game """
+    _name: str
+    _AMOUNT: int
+    _NUMBER: int = 0
+    _start: float = 0
+    _end: float = 0
+    _task_id: int = 0
+
+    async def calculate(self, event: Event) -> None:
+        """ Calculate """
+        # Wait for the event to be set before starting the calculation
+        await event.wait()
+
+        # Get the current task
+        self._task_id = id(current_task())
+
+        # Start the calculation
+        self._start = perf_counter()
+        for _ in range(self._AMOUNT):
+            self._NUMBER += 1
+            await sleep(0)
+        self._end = perf_counter()
+
+    def __str__(self) -> str:
+        return (f"{self._name:>12} {self._task_id}'s result is {self._NUMBER:>08d},"
+                f"elapsed {self._end - self._start:.6f} seconds.")
+
+
+async def main() -> None:
+    """ Main Function """
+    data_faker = Faker()
+    AMOUNT_NAME: int = 10
+    names: list[str] = [data_faker.first_name() for _ in range(AMOUNT_NAME)]
+
+    event_init = Event()
+
+    _tasks = []
+    _games = []
+    AMOUNT_DATA: int = 1_000_000
+    for name in names:
+        game = CalculationGame(name, AMOUNT_DATA)
+        _games.append(game)
+        task = create_task(game.calculate(event_init))
+        _tasks.append(task)
+        print(f"{name:>12} is ready to starting...")
+
+    print("Some event should be done before these tasks starting...")
+    await sleep(1)
+    print("Initialization complete!")
+
+    print("Event is set, tasks are starting...")
+    event_init.set()
+    await gather(*_tasks)
+
+    for game in _games:
+        print(game)
+
+
+if __name__ == "__main__":
+    run(main())
